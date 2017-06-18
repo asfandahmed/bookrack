e202f29e50773cb41a9bc85646d75fde68b33690
asfandahmed <asfandahmed1@gmail.com>
2017-06-18 12:55:46 +0500

    adding follow object.

---
+++ application_bookrack/models/Follow.php
@@ -0,0 +1,68 @@
+<?php 
+class Follow{
+	protected $CI;
+	public function __construct()
+	{
+		$this->CI  =& get_instance();
+		$this->CI->load->library('neo');
+	}
+	public function get_followers($label, $id)
+	{
+		$cypher = "MATCH (n:" . $label . " {id:{id}})<-[r:FOLLOWS]-(followers) 
+					WHERE r.approved = 1 
+					RETURN followers";
+		return $this->CI->neo->execute_query($cypher, array(
+			'id'=>intval($id)
+			)
+		);
+	}
+	public function get_following($label, $id)
+	{
+		$cypher = "MATCH (n:" . $label . " {id:{id}})-[r:FOLLOWS]->(following) 
+					WHERE r.approved = 1 
+					RETURN following";
+		return $this->CI->neo->execute_query($cypher, array(
+			'id'=>intval($id)
+			)
+		);
+	}
+	public function follow($parent_id, $follower_id)
+	{
+		$approved = 1;
+		$cypher = "MATCH (n {id:{parent_id}}), (m {id:{follower_id}}) 
+					CREATE UNIQUE (m)-[r:FOLLOWS {approved:{approved}, timestamp:{timestamp}}]->(n) 
+					RETURN r";
+		return $this->CI->neo->execute_query($cypher, array(
+			'parent_id'=>intval($parent_id),
+			'follower_id'=>intval($follower_id),
+			'approved'=>$approved
+			)
+		);
+	}
+	public function unfollow($parent_id, $follower_id)
+	{
+		$cypher = "MATCH (n {id:{follower_id}})-[r:FOLLOWS]->(f {id:{parent_id}}) 
+					DELETE r";
+		return $this->CI->neo->execute_query($cypher, array(
+			'parent_id'=>intval($parent_id),
+			'follower_id'=>intval($follower_id)
+			)
+		);
+	}
+	public function count_followers($id)
+	{
+		$cypher = "START n=node({id}) MATCH (n)<-[r:FOLLOWS]-() RETURN COUNT(r) AS followers";
+		return $this->CI->neo->execute_query($cypher, array(
+			'id'=>intval($id)
+			)
+		);
+	}
+	public function count_following($id)
+	{
+		$cypher = "START n=node({id}) MATCH (n)-[r:FOLLOWS]->() RETURN COUNT(r) AS following";
+		return $this->CI->neo->execute_query($cypher, array(
+			'id'=>intval($id)
+			)
+		);
+	}
+}
